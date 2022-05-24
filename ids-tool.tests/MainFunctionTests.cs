48a35c46b4443e6d246481d3c2491032495bd8e5
Claudio Benghi <claudio.benghi@gmail.com>
2022-05-24 16:48:36 +0200

    Adding net6 for github.

--- ids-tool.tests/MainFunctionTests.cs
+++ ids-tool.tests/MainFunctionTests.cs
@@ -14,14 +14,14 @@ namespace idsTool.tests
 		public void CanRunAsPublic()
 		{
 			CheckOptions c = new CheckOptions();
-			c.CheckSchema = new List<string> { @"C:\Data\Dev\BuildingSmart\IDS\Development\Third production release\ids.xsd" };
-			c.InputSource = @"C:\Data\Dev\XbimPrivate\Xbim.Xids\TestResults\saveattempt.xml";
+			c.CheckSchema = new List<string> { @"C:\Data\Dev\XbimPrivate\Xbim.Xids\Xbim.InformationSpecifications.NewTests\bsFiles\ids_06.xsd" };
+			c.InputSource = @"C:\Data\Dev\XbimPrivate\Xbim.Xids\Xbim.InformationSpecifications.NewTests\bsFiles\IDS_ucms_prefab_pipes_IFC2x3.xml";
 
 			// to adjust once we fix the xml file in the other repo.
 			var t = new StringWriter();
 			var ret = CheckOptions.Run(c, t);
 
-			Assert.Equal(Status.ContentError, ret);
+			Assert.Equal(Status.Ok, ret);
 		}
 	}
 }
